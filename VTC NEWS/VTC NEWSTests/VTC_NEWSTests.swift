2a2a0a8ced6c9135882f8eb67d6d2a8a38d0a721
hưng hoàng <hunghv3010@gmail.com>
2020-10-05 10:09:35 +0700

    update project

---
+++ VTC NEWS/VTC NEWSTests/VTC_NEWSTests.swift
@@ -0,0 +1,34 @@
+//
+//  VTC_NEWSTests.swift
+//  VTC NEWSTests
+//
+//  Created by hưng hoàng on 10/5/20.
+//  Copyright © 2020 hưng hoàng. All rights reserved.
+//
+
+import XCTest
+@testable import VTC_NEWS
+
+class VTC_NEWSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
